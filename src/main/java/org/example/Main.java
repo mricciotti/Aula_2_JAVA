c81631d0bdcfdd261114e76acbfe4b6a1b177b96
Matheus Ricciotti <matheus.ricciotti@gmail.com>
2025-02-18 19:59:15 -0300

    commit inicial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+//package com.example;
+//
+//public class Main {
+//    public static void main(String[] args){
+//        double pccf = 50.0;
+//        double gs = 70.0;
+//
+//        double ms1 = (pccf * 0.4) + (gs * 0.6);
+//
+//        System.out.println("O valor de MS1 é: " + ms1);
+//    }
+//}
+
+import java.util.Scanner;
+
+//public class CalcularMediaSemestral{
+//    public static void main (String[]args ){
+//        Scanner scanner = new Scanner(System.in);
+//        //Obter o valor de PCC&F e GS do usuario
+//        System.out.print("Digite o valor de PCC&F: ");
+//        double pccf = scanner.nextDouble();
+//
+//        System.out.printf("Digite o valor da GS: ");
+//        double gs= scanner.nextDouble();
+//
+//        //Calculo de MS1
+//        double ms1 = (pccf * 0.4) + (gs * 0.6);
+//
+//        //Exibe o resultado
+//        System.out.println("O valor de MS1 é de: " + ms1);
+//
+//        //Fecha o scanner
+//        scanner.close();
+//    }
+//}
